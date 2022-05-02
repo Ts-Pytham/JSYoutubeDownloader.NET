8822be56fd4d676dee63af292af413562ab59795
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-01 23:57:21 -0500

    Mejora visual (experimental)

---
+++ JSYoutubeDownloader.NET/Models/Video.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSYoutubeDownloader.NET.Models
+{
+    internal class Video
+    {
+    }
+}
