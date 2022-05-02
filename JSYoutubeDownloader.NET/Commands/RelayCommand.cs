8751107d3f88cd1e87f402539a170aa72b825dcf
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-01 21:09:44 -0500

    Agregar archivos de proyecto.

---
+++ JSYoutubeDownloader.NET/Commands/RelayCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JSYoutubeDownloader.NET.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action<object>? _execute;
+        private readonly Predicate<object>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+       
+        public void Execute(object? parameter)
+        {
+            if(parameter == null)
+              throw new ArgumentNullException(nameof(parameter));
+            if(_execute == null)
+                return;
+
+            _execute(parameter);
+        }
+    }
+}
---
+++ JSYoutubeDownloader.NET/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace JSYoutubeDownloader.NET.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+    }
+}
