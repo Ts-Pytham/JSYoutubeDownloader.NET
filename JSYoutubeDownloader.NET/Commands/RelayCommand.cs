64dc6927c83e1bfbcc83c4bce96b606809fe0b05
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-05-10 14:31:52 -0500

    Eliminación de using innecesarias

--- JSYoutubeDownloader.NET/Commands/RelayCommand.cs
+++ JSYoutubeDownloader.NET/Commands/RelayCommand.cs
@@ -1,48 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace JSYoutubeDownloader.NET.Commands
+namespace JSYoutubeDownloader.NET.Commands;
+
+internal class RelayCommand : ICommand
 {
-    internal class RelayCommand : ICommand
+    private readonly Action<object> _execute;
+
+    private readonly Func<object, bool>? _canExecute;
+
+    public event EventHandler? CanExecuteChanged
     {
-        private readonly Action<object> _execute;
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
 
-        private readonly Func<object, bool>? _canExecute;
+    public RelayCommand(Action<object> execute)
+    {
+        _execute = execute;
+        _canExecute = null;
+    }
 
-        public event EventHandler? CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
+    public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
 
-        public RelayCommand(Action<object> execute)
-        {
-            _execute = execute;
-            _canExecute = null;
-        }
+    
+    public bool CanExecute(object? parameter)
+    {
+        if (parameter == null)
+            return true;
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-        }
+        return _canExecute == null || CanExecute(parameter);
+    }
 
-        
-        public bool CanExecute(object? parameter)
-        {
-            if (parameter == null)
-                return true;
-
-            return _canExecute == null || CanExecute(parameter);
-        }
-
-        public void Execute(object? parameter)
-        {
-            _execute(parameter!);
-        }
+    public void Execute(object? parameter)
+    {
+        _execute(parameter!);
     }
 }
