c81260ca6af1b0598c77187d2d17474f6d8dd401
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-07-20 18:33:48 -0500

    Implementación de QueryKeyEvent

--- JSYoutubeDownloader.NET/Commands/Querys/IQuery.cs
+++ JSYoutubeDownloader.NET/Commands/Querys/IQuery.cs
@@ -1,6 +1,7 @@
 ﻿namespace JSYoutubeDownloader.NET.Commands;
 
-public interface IQuery<T>
+public interface IQuery<T> where T : EventArgs
 {
-
+    public object? Sender { get; set; }
+    public T Args { get; set; }
 }
---
+++ JSYoutubeDownloader.NET/Commands/Querys/QueryKeyEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace JSYoutubeDownloader.NET.Commands;
+
+public class QueryKeyEvent : IQuery<KeyEventArgs>
+{
+    public object? Sender { get; set; }
+    public KeyEventArgs Args { get; set; }
+
+    public QueryKeyEvent(KeyEventArgs args)
+    {
+        Args = args;
+    }
+}
